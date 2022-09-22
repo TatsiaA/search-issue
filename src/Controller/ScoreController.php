a39c92f52611b9978e5f3c27467753cc5ebe3caf
TatsiaA <taniokk@mail.ru>
2022-09-22 11:38:31 +0300

    tmp

---
+++ src/Controller/ScoreController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Word;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ScoreController extends AbstractController
+{
+    public HttpClientInterface $client;
+
+    /**
+     * @param HttpClientInterface $client
+     */
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    #[Route('/score', name: 'app_score', methods: ['GET'])]
+    public function score(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $term = $request->get('term');
+
+        $repository = $doctrine->getRepository(Word::class);
+        $query = $repository->createQueryBuilder('w')
+            ->where('w.term = :term')
+            ->setParameter('term', $term)
+            ->getQuery()
+            ->getResult();
+
+        if (!$query) {
+            $contentSucks = $this->getContent($term);
+            $contentRocks = $this->getContent($term, 'rocks');
+            $score = $contentRocks['total_count'] / ($contentSucks['total_count'] + $contentRocks['total_count']) * 10;
+
+            $query = new Word();
+            $query->setTerm($term);
+            $query->setScore($score);
+            $query->setCreatedAt(new \DateTimeImmutable(''));
+            $query->setUpdatedAt(new \DateTimeImmutable(''));
+
+            $repository->add($query, true);
+        } else {
+            $score = $query[0]->getScore();
+        }
+        $output = [
+            'term' => $term,
+            'score' => round($score, 2)
+        ];
+        return $this->json($output);
+
+    }
+
+    /**
+     * @param string $term
+     * @param string $additionalParam
+     * @return array
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function getContent(string $term, string $additionalParam = 'sucks'): array {
+        $response = $this->client->request(
+            'GET',
+            'https://api.github.com/search/issues?q=' . $term . '+' . $additionalParam .  '&sort=created&order=asc'
+        );
+
+        $response->getContent();
+
+        return $response->toArray();
+    }
+}
---
+++ src/Entity/Word.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WordRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WordRepository::class)]
+class Word
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $term = null;
+
+    #[ORM\Column]
+    private ?float $score = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTerm(): ?string
+    {
+        return $this->term;
+    }
+
+    public function setTerm(string $term): self
+    {
+        $this->term = $term;
+
+        return $this;
+    }
+
+    public function getScore(): ?float
+    {
+        return $this->score;
+    }
+
+    public function setScore(float $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    public function prePersist(): void
+    {
+        $this->setUpdatedAt(new \DateTimeImmutable('now'));
+        if ($this->getCreatedAt() === null) {
+            $this->setCreatedAt(new \DateTimeImmutable('now'));
+        }
+    }
+    #[ORM\PreUpdate]
+    public function preUpdate(): void
+    {
+        $this->setUpdatedAt(new \DateTimeImmutable('now'));
+        if ($this->getCreatedAt() === null) {
+            $this->setCreatedAt(new \DateTimeImmutable('now'));
+        }
+    }
+}
