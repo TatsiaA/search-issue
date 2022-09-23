1c5dcf6f1a43dc99c08bd80febf3cbd782c402e5
TatsiaA <taniokk@mail.ru>
2022-09-23 22:13:13 +0300

    Refactor

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -2,35 +2,25 @@
 
 namespace App\Controller;
 
-use App\Service\StoreService;
+use App\Service\ScoreService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ScoreController extends AbstractController
 {
-
-    /**
-     * @param HttpClientInterface $client
-     */
-    public function __construct(public readonly HttpClientInterface $client)
-    {
-    }
-
     /**
      * @param Request $request
-     * @param StoreService $storeService
+     * @param ScoreService $scoreService
      * @return Response
      */
     #[Route('/score', name: 'app_score', methods: ['GET'])]
-    public function score(Request $request,StoreService $storeService): Response
+    public function score(Request $request, ScoreService $scoreService): Response
     {
         $term = $request->get('term');
         $provider = $request->get('provider');
 
-        return $this->json($storeService->findWord($this->client, $term, $provider));
+        return $this->json($scoreService->rating($term, $provider));
     }
-
 }
