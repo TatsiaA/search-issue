38d77a1e910eca9008591ccae50fc0ad66ec0c02
TatsiaA <taniokk@mail.ru>
2022-09-26 11:49:52 +0300

    Tests

---
+++ tests/Controller/ScoreControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\ScoreController;
+use App\Service\ScoreService;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ScoreControllerTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testScoreService(): void
+    {
+        $service = $this->createMock(ScoreService::class);
+        $service->method('rating')
+            ->willReturn([]);
+
+        $score = new ScoreController();
+        $score->setContainer($this->createStub(ContainerInterface::class));
+
+        $this->assertInstanceOf(JsonResponse::class, new JsonResponse());
+    }
+}
